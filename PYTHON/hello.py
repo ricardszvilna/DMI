a0e38622aaf7ad2dad046f0a0bf48a3d1b1f5092
rastamaha1998@gmail.com <ricards.toms.zvilna@gmail.com>
2017-11-08 14:12:18 +0200

    20171108_14_10

---
+++ PYTHON/hello.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# echo -e 'here the \vspaces \vhave \vvertical \vtab \vspaces'
+print "here the  \vkrīt \vvāja \vrudens \vlapa \vun \vkāja \vtai \vir \vslapja. \v\b\b\b\b\b\b\b\b\b\b\b\bMana \v\b\b\b\b\b\b\b\b\b\b\b\b\bdvēsele \v\b\b\b\b\b\b\b\b\b\b\bko \v\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bsaka \v\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bpar \v\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bvājo \v\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\brudens \v\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\blapu..."
+
+# echo -e 'here the \rcontent before this is removed'
+print "here the \rcontent before this is removed"
+
+# echo -e 'here \vthe \vspaces \vhave \vvertical \vtab \vspaces.'
+print "here \vthe \vspaces \vhave \vvertical \vtab \vspaces."
+
+# echo -e 'here \tthe \tspaces \thave \thorizontal \ttab \tspaces'
+print "here \tthe \tspaces \thave \thorizontal \ttab \tspaces"
+
+# echo -e 'Here \nthe \nspaces \nare \nnewlined.'
+print "here \nthe \nspaces \nare \nnewlined."
+
+# https://www.tutorialspoint.com/unix_commands/echo.htm
+# example 3. -7.
+
+# example 3
+# echo -e 'Here \bthe \bspaces \bare \bbackspace.'
+print "here \bthe \bspace \bare \bbackspace."
+
+'''
+# 1. piemērs
+print "hello world"
+'''
+
---
+++ PYTHON/variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#a=10
+#echo "mainīgā a veritiba ir $a"
+
+a=78
+#https://cplusplus.com ja kkas nav skaidrs
+print type(a) 
+print "mainīgā a vēritība ka skaitlis ir %d"%(a)
+print "mainīgā a vēritība ka skaitlis ir %x"%(a)
+print "mainīgā a vēritība ka skaitlis ir %o"%(a)
+print "mainīgā a vēritība ka simbols ir %c"%(a)
+
