a035a831531d4996de34a94d38fb3e92d248c17b
rastamaha1998@gmail.com <ricards.toms.zvilna@gmail.com>
2017-11-22 16:10:09 +0200

    20171122_16_10

---
+++ PYTHON/cosh_caur_sum.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from math import cosh
+ 
+ These commands were executed: >>> 
+ from __future__ import division 
+ from sympy import *  
+ x, y, z, t = symbols('x y z t') 
+ k, m, n = symbols('k m n', integer=True) 
+ f, g, h = symbols('f g h', cls=Function) 
+ from sympy.mpmath import *
+ Traceback (most recent call last): File "<string>", line 1, in <module> ImportError: No module named mpmath
+
+ mp.dps = 25; mp.pretty = True
+ Traceback (most recent call last): File "<string>", line 1, in <module> NameError: name 'mp' is not defined 
+
+
+
+
+nekas shitais nav pareizi 
+
